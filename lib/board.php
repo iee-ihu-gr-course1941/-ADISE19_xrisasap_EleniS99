cef7aaa9352e812605cb215896c2f11fc2d129a1
EleniS99 <57598412+EleniS99@users.noreply.github.com>
2020-01-05 20:07:16 +0200

    Update board.php

--- lib/board.php
+++ lib/board.php
@@ -5,11 +5,15 @@ function show_board() {
 	
 	$sql = 'select * from board';
 	$st = $mysqli->prepare($sql);
+	
 	$st->execute();
 	$res = $st->get_result();
+	
 	header('Content-type: application/json');
 	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+	
 }
+
 function reset_board() {
 	global $mysqli;
 	
