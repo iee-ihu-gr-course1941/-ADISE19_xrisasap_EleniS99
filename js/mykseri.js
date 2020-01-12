9e3e498acb829e7d64dea65c86b6a6e48605d489
xrisasap <57598027+xrisasap@users.noreply.github.com>
2020-01-12 17:41:23 +0200

    Update mykseri.js

--- js/mykseri.js
+++ js/mykseri.js
@@ -1,37 +1,25 @@
-var me={};
-var game_status={};
-
 $(function () {
 	draw_empty_board();
 	fill_board();
-	
-	$('#kseri_login').click( login_to_game);
-	$('#kseri_reset').click( reset_board);
-	$('#move_div').hide();
-	game_status_update();
 });
 
+
 function draw_empty_board() {
-	if(p!='2') {p='1';}
-	var draw_init = {
-		'1': {i1:6,i2:0,istep:-1,j1:1,j2:6,jstep:1},
-		'2': {i1:1,i2:6,istep:1, j1:6,j2:0,jstep:-1}
-	};
-	var t='<table id="kseri_table">';
-	for(var i=6;i>0;i--) {
+	var t='<table id="chess_table">';
+	for(var i=8;i>0;i--) {
 		t += '<tr>';
-		for(var j=1;j<6;j++) {
-			t += '<td class="kseri_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
+		for(var j=1;j<9;j++) {
+			t += '<td class="chess_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
 		}
 		t+='</tr>';
 	}
 	t+='</table>';
 	
-	$('#kseri_board').html(t);
+	$('#chess_board').html(t);
 }
 
 function fill_board() {
-	$.ajax({url: "kseri.php/board/", success: fill_board_by_data });
+	$.ajax({url: "chess.php/board/", success: fill_board_by_data });
 	
 }
 
@@ -39,9 +27,9 @@ function fill_board_by_data(data) {
 	for(var i=0;i<data.length;i++) {
 		var o = data[i];
 		var id = '#square_'+ o.x +'_' + o.y;
-		var c = (o.filla!=null)?o.color + o.filla:'';
-		var im = (o.filla!=null)?'<img class="filla" src="model/'+c+'.png">':'';
-		$(id).addClass(o.table_color+'_square').html(im);
+		var c = (o.piece!=null)?o.piece_color + o.piece:'';
+		var im = (o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
+		$(id).addClass(o.b_color+'_square').html(im);
 		
 	}
 }
