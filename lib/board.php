b6dad44a2b0a832c079f220f5f86e66e950f616c
EleniS99 <57598412+EleniS99@users.noreply.github.com>
2020-01-12 18:42:11 +0200

    Update board.php

--- lib/board.php
+++ lib/board.php
@@ -1,19 +1,47 @@
 <?php
-function show_board() {
-	
+function show_card($x,$y) {
 	global $mysqli;
 	
-	$sql = 'select * from board';
+	$sql = 'select * from board where x=? and y=?';
 	$st = $mysqli->prepare($sql);
+	$st->bind_param('ii',$x,$y);
 	$st->execute();
 	$res = $st->get_result();
 	header('Content-type: application/json');
 	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+  
+function show_board($input) {
+	global $mysqli;
+	
+	$b=current_color($input['token']);
+	if($b) {
+		show_board_by_player($b);
+	} else {
+		header('Content-type: application/json');
+		print json_encode(read_board(), JSON_PRETTY_PRINT);
+	}
 }
 function reset_board() {
 	global $mysqli;
 	
 	$sql = 'call clean_board()';
 	$mysqli->query($sql);
+	
+}
+function read_board() {
+	global $mysqli;
+	$sql = 'select * from board';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	return($res->fetch_all(MYSQLI_ASSOC));
+}
+function convert_board(&$orig_board) {
+	$board=[];
+	foreach($orig_board as $i=>&$row) {
+		$board[$row['x']][$row['y']] = &$row;
+	} 
+	return($board);
 }
 ?>
