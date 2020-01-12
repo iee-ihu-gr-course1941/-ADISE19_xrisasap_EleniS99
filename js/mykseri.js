a25be34006d4034fda0260129e80c1ff74666c3e
xrisasap <57598027+xrisasap@users.noreply.github.com>
2020-01-12 18:16:17 +0200

    Update mykseri.js

--- js/mykseri.js
+++ js/mykseri.js
@@ -6,7 +6,7 @@ $(function () {
 
 function draw_empty_board() {
 	var t='<table id="kseri_table">';
-	for(var i=6;i>0;i--) {
+	for(var i=5;i>0;i--) {
 		t += '<tr>';
 		for(var j=1;j<6;j++) {
 			t += '<td class="kseri_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
