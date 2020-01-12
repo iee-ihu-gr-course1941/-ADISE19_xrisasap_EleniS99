bf5dbc1b0c1231f9e207d7c992167f456b194026
EleniS99 <57598412+EleniS99@users.noreply.github.com>
2020-01-12 18:36:02 +0200

    Update mykseri.js

--- js/mykseri.js
+++ js/mykseri.js
@@ -6,9 +6,9 @@ $(function () {
 
 function draw_empty_board() {
 	var t='<table id="kseri_table">';
-	for(var i=5;i>0;i--) {
+	for(var i=6;i>0;i--) {
 		t += '<tr>';
-		for(var j=1;j<6;j++) {
+		for(var j=1;j<5;j++) {
 			t += '<td class="kseri_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
 		}
 		t+='</tr>';
